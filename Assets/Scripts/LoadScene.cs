81bd0d4511315a7abc11a2617ee66dacd9932ae3
DW Chen <tsukasa800921@gmail.com>
2022-10-30 18:51:24 +0800

    feat: create a app start scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadAsyncScene(string sceneName)
+    {
+        Debug.Log("LoadAsyncScene onClick");
+        StartCoroutine(loadAsyncSceneInternal(sceneName));
+        Debug.Log("LoadAsyncScene onClick done");
+    }
+
+    private IEnumerator loadAsyncSceneInternal(string sceneName)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        Debug.Log("start to load scene");
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        Debug.Log("done to load scene");
+    }
+}
